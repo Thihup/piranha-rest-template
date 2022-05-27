8291c065f7435841b9158bc56830c58cfc509ea4
Thiago Henrique Hupner <thihup@gmail.com>
2022-05-26 23:32:58 -0300

    Remove ApplicationScoped from JsonbConfigurator

--- src/main/java/dev/thihup/piranha/rest/template/config/JsonbConfigurator.java
+++ src/main/java/dev/thihup/piranha/rest/template/config/JsonbConfigurator.java
@@ -1,13 +1,11 @@
 package dev.thihup.piranha.rest.template.config;
 
-import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
 import jakarta.json.bind.JsonbConfig;
 import jakarta.ws.rs.ext.ContextResolver;
 import jakarta.ws.rs.ext.Provider;
 
-@ApplicationScoped
 @Provider
 public class JsonbConfigurator implements ContextResolver<Jsonb> {
 
